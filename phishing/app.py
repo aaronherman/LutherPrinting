5a8805cdd2b7f0f0a6dd030c0dd68d07c41affc0
aaronherman <aaron@aaronherman.co>
2018-05-02 19:15:09 -0500

    add .env and sendgrid

--- phishing/app.py
+++ phishing/app.py
@@ -5,6 +5,8 @@ import psycopg2
 from flask import Flask, jsonify, render_template, redirect, url_for,g, jsonify
 from flask import request
 import random
+import sendgrid
+from sendgrid.helpers.mail import *
 
 app = Flask(__name__)
 app.debug = True
@@ -44,6 +46,17 @@ def get_db():
 	if not hasattr(g, 'envelope'):
 		g.envelope = connect_db()
 	return g.envelope
+
+def sendemail(email):
+	sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+	from_email = Email("no-reply@printluther.com")
+	to_email = Email(email)
+	subject = "ENTER SUBJECT HERE"
+
+	#NOTE: sendgrid allows you to create and export HTML. Use that to generate HTML for the content?
+	content = Content("text/html", """<a href="https://pathwerks.herokuapp.com/verifyuser/"""+token+"""">Please click this link to verify your account on Pathwerks.com</a>""")
+	mail = Mail(from_email, subject, to_email, content)
+	response = sg.client.mail.send.post(request_body=mail.get())
 
 @app.route('/')
 def home():
